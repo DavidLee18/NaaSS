40b7f6a51aca04fc40fc8e09522110a617707f91
David Lee <djwodus@gmail.com>
2021-10-08 10:52:44 +0900

    login sketch

--- backend/substream/main/views.py
+++ backend/substream/main/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
-
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
+from django.contrib.auth import authenticate, login, logout
+from django.http.response import HttpResponse, JsonResponse
+from django.utils.datastructures import MultiValueDictKeyError
 from rest_framework import status
- 
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 from .models import Profile
 from .serializers import ProfileSerializer
-from rest_framework.decorators import api_view
 
+#profile api
 
 @api_view(['GET'])
 def profiles(request):
@@ -36,3 +36,31 @@ def profile_edit(request, id):
             return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Profile.DoesNotExist: 
         return JsonResponse({'message': 'The profile does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+# auth api
+
+@api_view(['POST'])
+def log_in(request):
+    try:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            p = Profile.objects.get(user=user)
+            return JsonResponse(ProfileSerializer(p).data)
+        else:
+            return JsonResponse({
+                'message': 'username or password invalid or doesn\'t match any user'
+            }, status=status.HTTP_400_BAD_REQUEST)
+    except MultiValueDictKeyError:
+        print(request.POST)
+        return JsonResponse({'message': 'no username or password found'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def log_out(request):
+    try:
+        logout(request)
+        return HttpResponse(status=status.HTTP_200_OK)
+    except:
+        return JsonResponse({'message': 'error during logout'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
