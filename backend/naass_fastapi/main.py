c739852c03e7c5cf604e11cdf2c8f2e7a39d1f8d
이재현 <djwodus@gmail.com>
2021-10-29 17:24:43 +0900

    fix typo

--- backend/naass_fastapi/main.py
+++ backend/naass_fastapi/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_heasers=["*"]
+    allow_headers=["*"]
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
