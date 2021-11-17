de3b7ceaf5cb6c4cd96536016ff98e07d415c467
이재현 <djwodus@gmail.com>
2021-11-17 09:30:58 +0900

    mute too-verbose error

--- frontend/substream-client/src/functions.js
+++ frontend/substream-client/src/functions.js
@@ -15,23 +15,23 @@ export const handleError = (error) => {
   if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
-      console.error(`\\response data: ${JSON.stringify(error.response.data)}`);
-      console.error(`\\response status: ${error.response.statusText} (${error.response.status})`);
-      console.error(`\\response headers: ${JSON.stringify(error.response.headers)}`);
+      // console.error(`\\response data: ${JSON.stringify(error.response.data)}`);
+      // console.error(`\\response status: ${error.response.statusText} (${error.response.status})`);
+      // console.error(`\\response headers: ${JSON.stringify(error.response.headers)}`);
 
       store.dispatch('setError', JSON.stringify(error.response.data));
   } else if (error.request) {
       // The request was made but no response was received
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
-      console.error(`\\request: ${JSON.stringify(error.request)}`);
+      // console.error(`\\request: ${JSON.stringify(error.request)}`);
 
       store.dispatch('setError', error.message);
   } else {
       // Something happened in setting up the request that triggered an Error
-      console.error(`\\message: ${error.message}`);
+      // console.error(`\\message: ${error.message}`);
 
       store.dispatch('setError', error.message);
   }
-  console.error(`\\config: ${JSON.stringify(error.config)}`);
+  // console.error(`\\config: ${JSON.stringify(error.config)}`);
 };
