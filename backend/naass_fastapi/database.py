7e634c10d485635998d30baceeb0d13afb2f7a66
이재현 <djwodus@gmail.com>
2021-11-17 11:29:03 +0900

    from test to production db

--- backend/naass_fastapi/database.py
+++ backend/naass_fastapi/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #mysql+pymysql://{username}:{password}@{host}:{port}/{db}
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://naass_user:mail1234@192.168.251.251:3306/naass_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://naass_user:NaaSS!%40#$0987@141.164.45.196:3306/naass_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=300)
 
